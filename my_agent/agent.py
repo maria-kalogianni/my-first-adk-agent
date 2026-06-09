aa52c2516259b4ef3a28ff9a5b479d687b7699f8
Maria Kalogianni <maria.kalogianni@satalia.com>
2026-06-09 15:27:31 +0300

    Chandeg instruction in order to use better the search_tool

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -5,6 +5,7 @@ root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
     description='A helpful assistant that can search the web.',
-    instruction='Answer user questions to the best of your knowledge. When you need up-to-date information, use the google_search tool.',
+    #instruction='Answer user questions to the best of your knowledge. When you need up-to-date information, use the google_search tool.',
+    instruction='Always use the google_search tool for any question about current date, time, weather, news, or any real-time information. Never answer these from memory.',
     tools=[google_search],
 )
